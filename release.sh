88aa13c4038cc1d041b611fe9af69c8d3f3f18a6
lucasart <lucas.braesch@gmail.com>
2014-08-24 15:06:18 +0800

    fix release script

--- release.sh
+++ release.sh
@@ -1,15 +1,9 @@
-W="-Wall -Wextra -pedantic -Wshadow"
+FLAGS="-DNDEBUG -std=c++11 -O3 -fno-rtti -flto -s -Wall -Wextra -pedantic -Wshadow"
 
 echo "building linux compiles"
-g++ ./src/*.cc -o ./bin/${1}_sse2   -DNDEBUG -std=c++11 -O3 -msse2   -fno-rtti -flto -s $W
-g++ ./src/*.cc -o ./bin/${1}_sse4.2 -DNDEBUG -std=c++11 -O3 -msse4.2 -fno-rtti -flto -s $W
+g++ ./src/*.cc -o ./bin/${1}_x86-64        -msse2          ${FLAGS}
+g++ ./src/*.cc -o ./bin/${1}_x86-64_popcnt -msse3 -mpopcnt ${FLAGS}
 
 echo "building windows compiles"
-x86_64-w64-mingw32-g++ ./src/*.cc -o ./bin/${1}_sse2.exe   -DNDEBUG -std=c++0x -O3 -msse2   -fno-rtti -s -static -flto $W
-x86_64-w64-mingw32-g++ ./src/*.cc -o ./bin/${1}_sse4.2.exe -DNDEBUG -std=c++0x -O3 -msse4.2 -fno-rtti -s -static -flto $W
-
-echo "make tarball and cleanup"
-cd ./bin
-tar czf ./${1}.tar.gz ./${1}_sse*
-rm ./${1}_sse*
-cd ..
+x86_64-w64-mingw32-g++ ./src/*.cc -o ./bin/${1}_x86-64.exe        -msse2          ${FLAGS} -static
+x86_64-w64-mingw32-g++ ./src/*.cc -o ./bin/${1}_x86-64_popcnt.exe -msse3 -mpopcnt ${FLAGS} -static
